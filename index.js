3d2b985c95147fdb03816e02fb25067a83ff8470
kosatun@aol.com <kosatun@aol.com>
2018-06-29 03:24:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,23 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+  }
+   return "I can't hear you!"
+}
+   
+function sayHiToGrandma(string) {   
+  if (string.toUpperCase === string) {
+  }    
+    return "YES INDEED."
+}   
 
+ function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+  }  
+    return "I love you, too."
+}
 
 
 
